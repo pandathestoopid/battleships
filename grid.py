427bfd022511f6e039d79bd6b41772920779346b
pandathestoopid <jonahlovesraspberries@gmail.com>
2025-06-04 12:44:39 +1200

    Added README.md

--- grid.py
+++ grid.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from functools import partial
 
+from ships import *
+
 root = Tk()
 root.geometry('900x600')
 root.title('Battleships')
@@ -10,12 +12,18 @@ root.resizable(False, False)
 squares = []
 
 # Test, value should be false
-shipsPlaced = True
+shipsPlaced = False
 
 
 class Grid:
     def __init__(self, size):
         self.size = size
+
+        # 2D list of buttons for later reference
+        self.buttons = [[None for _ in range(size)] for _ in range(size)]
+
+        # Ship preview options
+        self.shipSize = 3  # This is a placeholder
 
     # Locks a square the user clicked on
     def click(self, x, y):
@@ -24,6 +32,14 @@ class Grid:
         else:
             userGrid.lock(x, y)
         return
+
+    # When the mouse hovers over a button
+    def on_button_enter(self, event):
+        event.widget.config(bg="gray")
+
+    # When the mouse is not hovering over a button
+    def on_button_leave(self, event):
+        event.widget.config(bg="#eee")
 
     # Generates the grid with its buttons
     def generate(self, framePos):
@@ -34,17 +50,22 @@ class Grid:
 
         # Adds the buttons to the grid
         letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-        yNum = 0
-        xNum = 0
+
         for row in range(self.size):
-            for square in range(self.size):
+            for col in range(self.size):
                 square = Button(
-                    root, bg='#eee', activebackground='#ddd', width=3, height=1, relief='ridge', command=lambda y=yNum+1, x=letter[xNum]: self.click(x, y))
-                square.grid(column=yNum, row=xNum)
+                    gridFrame, bg='#eee', activebackground='#ddd', width=3, height=1, relief='ridge', command=lambda y=row+1, x=letter[col]: self.click(x, y))
+
+                square.grid(column=col, row=row)
+
+                # Bind the hovering methods
+                square.bind("<Enter>", self.on_button_enter)
+                square.bind("<Leave>", self.on_button_leave)
+
+                # Store buttons in 1 and 2D list
+                self.buttons[row][col] = square
                 squares.append(square)
-                yNum += 1
-            yNum = 0
-            xNum += 1
+
         print(squares)
 
     def placeShip(self, x, y):
